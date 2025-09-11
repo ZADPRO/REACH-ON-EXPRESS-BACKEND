dcf5f5b85e0e04a30d1ed79a3918e2d66645a294
ionhax-dev <thirukumara.d@zadroit.com>
2025-09-11 17:24:18 +0530

    backend query updated

--- src/api/user/query.ts
+++ src/api/user/query.ts
@@ -4,17 +4,16 @@ export const UserLoginQuery = `
 `;
 
 export const parcelDetailsPaginated = `
-  SELECT *
+SELECT *
   FROM public."bulkParcelDataMapping" bpc
   WHERE bpc.dsr_value = $1
     AND (
-      $4 = '' OR
       bpc.dsr_cnno ILIKE '%' || $4 || '%' OR
       bpc.dsr_dest ILIKE '%' || $4 || '%' OR
       bpc.dsr_dest_pin::text ILIKE '%' || $4 || '%' OR
       bpc.dsr_contents ILIKE '%' || $4 || '%'
     )
-  ORDER BY bpc.id ASC
+  ORDER BY bpc.dsr_booking_date ASC
   LIMIT $2 OFFSET $3;
 `;
 
